e969c54d09ee1bcc1e1d3e1ec2951f7e194a8981
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-06-22 17:47:52 +0300

    sync

--- src/cmps/landing-preview.jsx
+++ src/cmps/landing-preview.jsx
@@ -6,10 +6,7 @@ export const LandingPreview = ({ landing }) => {
     const date = new Intl.DateTimeFormat("en-US").format(landing.date)
     let desc = landing.details
     return <div className="landing-preview">
-        <div>
-            {landing.links.youtube_id && <iframe src={`http://www.youtube.com/watch?v=${landing.links.youtube_id}`} frameborder="0"></iframe>}
-            {!landing.links.youtube_id && <img src={landing.links.patch.small} alt={`${landing.name} img`} />}
-        </div>
+        <img src={landing.links.patch.small} alt={`${landing.name} img`} />
         <h1>{landing.name}</h1>
         <span>{date}</span>
         {!desc && <p>No details yet</p>}
